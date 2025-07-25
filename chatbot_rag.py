dc8d0cd09eda9d9e9db53ee8d3172768374cb839
Alex <alexanderlinus@gmail.com>
2025-07-25 10:26:35 -0700

    changed chatbot title

--- chatbot_rag.py
+++ chatbot_rag.py
@@ -14,7 +14,7 @@ from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 
 load_dotenv()
 
-st.title("Chatbot")
+st.title("Cashflow Depot Chatbot")
 
 # initialize pinecone database
 pc = Pinecone(api_key=os.environ.get("PINECONE_API_KEY"))
