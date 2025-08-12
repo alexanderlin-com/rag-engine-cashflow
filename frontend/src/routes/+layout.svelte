2fe1487b516a4c4d54ccc708124e33e26a70069e
Alex <alexanderlinus@gmail.com>
2025-08-11 22:55:27 -0700

    nuking frontend. starting over

--- frontend/src/routes/+layout.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  import '../app.css'; // This imports our Tailwind directives.
-</script>
-
-<div class="min-h-screen bg-white dark:bg-black text-black dark:text-white font-sans">
-  <slot />
-</div>
